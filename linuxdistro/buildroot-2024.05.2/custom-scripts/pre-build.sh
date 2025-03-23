552a95fd92ffcf4faf5ead0e9620bdf341a03e1f
Andriele Barcé Lange <115933269+AndrieleLange@users.noreply.github.com>
2025-03-23 01:53:32 +0000

    tem algo errado

--- linuxdistro/buildroot-2024.05.2/custom-scripts/pre-build.sh
+++ linuxdistro/buildroot-2024.05.2/custom-scripts/pre-build.sh
@@ -3,5 +3,6 @@
 HOST=`hostname -I | awk '{print $1}'`
 cat $BASE_DIR/../custom-scripts/network-config | sed 's/\[IP-DO-HOST\]/'"$HOST"'/g' > $BASE_DIR/../custom-scripts/S41network-config
 cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
-cp $BASE_DIR/../custom-scripts/
+cp $BASE_DIR/../custom-scripts/apps/teste.c $BASE_DIR/target/etc/init.d
 chmod +x $BASE_DIR/target/etc/init.d/S41network-config
+chmod +x $BASE_DIR/target/etc/init.d/teste.c
